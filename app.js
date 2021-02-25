99e63510124afdd97fb9cf2c9b970104e1b6274d
JONATHAN MORELAND <jonathanmoreland@JONATHANs-MacBook-Air.local>
2021-02-24 18:33:13 -0800

    CSS progress

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ form.addEventListener('submit', getValue)
 async function getData(selectedCurrency) {
   const dataURL = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${selectedCurrency}`
   try {
+    removeInfo()
     const response = await axios.get(dataURL)
     const coin = response.data[0]
     showCoinInfo(coin)
@@ -36,45 +37,45 @@ async function getData(selectedCurrency) {
     coinContainer.insertAdjacentHTML('beforeend', coinInfo)
   }
 
-  // function removeInfo() {
-  //   const removeDiv = documment.querySelector("#currency-info")
-  //   while (removeDiv.lastChild) {
-  //     removeDiv.removeChild(removeDiv.lastChild)
-  //   }
-  // }
-  // removeInfo()
-  
-  
-  async function getTrend() {
-    const trendURL = 'https://api.coingecko.com/api/v3/search/trending'
-    try {
-      const response = await axios.get(trendURL)
-      const trend = response.data[0]
-      showTrendInfo(trend)
-      console.log(trend)
-      return response
-
-    } catch (err) {
-      console.error(err)
+  function removeInfo() {
+    const coinContainer = document.querySelector("#currency-info")
+    while (coinContainer.lastChild) {
+      coinContainer.removeChild(coinContainer.lastChild)
     }
-    
-    window.onload = function showTrendInfo(trend) {
-      console.log('here', trend)
-      const trendContainer = document.querySelector("#hot-coins")
-      console.log(trendContainer)
-      const trendInfo = `
-        <h1 class= trendName >Name: ${trend.name}</h1>
-        // <h2 class= currentPrice >Current Price: $${trend.current_price}</h2>
-        // <p class = priceChange >Price Change Last 24Hrs: $${trend.price_change_24h}</p>
-        // <p class = high >24Hr High: $${trend.high_24h}</p>
-        // <p class = low >24Hr Low: $${trend.low_24h}</p>
-      `
-      trendContainer.insertAdjacentHTML('beforeend', trendInfo)
-      console.log('trend', trendInfo)
-    }
-
   }
-  getTrend()
+  
+  
+  
+  // async function getTrend() {
+  //   const trendURL = 'https://api.coingecko.com/api/v3/search/trending'
+  //   try {
+  //     const response = await axios.get(trendURL)
+  //     const trend = response.data[0]
+  //     showTrendInfo(trend)
+  //     console.log(trend)
+  //     return response
+
+  //   } catch (err) {
+  //     console.error(err)
+  //   }
+    
+  //   window.onload = function showTrendInfo(trend) {
+  //     console.log('here', trend)
+  //     const trendContainer = document.querySelector("#hot-coins")
+  //     console.log(trendContainer)
+  //     const trendInfo = `
+  //       <h1 class= trendName >Name: ${trend.name}</h1>
+  //       // <h2 class= currentPrice >Current Price: $${trend.current_price}</h2>
+  //       // <p class = priceChange >Price Change Last 24Hrs: $${trend.price_change_24h}</p>
+  //       // <p class = high >24Hr High: $${trend.high_24h}</p>
+  //       // <p class = low >24Hr Low: $${trend.low_24h}</p>
+  //     `
+  //     trendContainer.insertAdjacentHTML('beforeend', trendInfo)
+  //     console.log('trend', trendInfo)
+  //   }
+
+  // }
+  // getTrend()
 
 
 }
